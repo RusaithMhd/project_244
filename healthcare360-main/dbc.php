972349bf9a8b7a17144881313c4493bf66c31746
RusaithMhd <111362210+RusaithMhd@users.noreply.github.com>
2024-01-20 00:21:27 +0530

    Add files via upload

---
+++ healthcare360-main/dbc.php
@@ -0,0 +1,9 @@
+<?php
+$con = mysqli_connect("localhost","root","","project_244");
+
+if ($con == false) {
+    die("CONNECTION ERROR". mysqli_connect_error());
+}
+
+?>
+
---
+++ healthcare360-main/index.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Mentel health care 360'</title>
+    <link rel="stylesheet" href="fontawesome-free-5.15.3-web/css/all.min.css">
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
+    <link rel="icon" href="my images/top.png">
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+
+</head>
+
+<body>
+
+    <header class="header">
+        <img src="my images/logo (2).png" id="logo" alt="logo">
+        <nav class="navbar">
+            <a href="#home">home</a>
+            <a href="#about">about</a>
+            <a href="#services">services</a>
+            <a href="#gallery">gallery</a>
+            <a href="#contact">contact</a>
+        </nav>
+        <div class="icons">
+            <div id="menubar" class="fas fa-bars"></div>
+            <a href="register.php">Appointment</a>
+        </div>
+    </header>
+
+    <div id="home" class="background-image">
+        <div class="background-content">
+            <h1>Mental Health Care <br> 360</h1>
+            <p>At SelfCare360, we believe that mental wellness is a journey, <br>
+                and we're here to guide you on that path to inner peace and self-empowerment.<br>
+                In a world that often demands more than it gives, it's crucial to take a moment for yourself,<br>
+                to prioritize self-care, and to discover the strength that lies within.</p>
+        </div>
+    </div>
+
+    <div id="about" class="about">
+        <div class="main-about">
+            <div class="inner-about">
+                <div class="about-content">
+                    <h1>about us</h1>
+                    <p>At SelfCare360, we are passionate about empowering individuals to prioritize and enhance their
+                        mental wellness.<br>
+                        Our mission is to be your trusted partner on your journey to improved emotional health, <br>
+                        self-awareness, and overall well-being.</p>
+                </div>
+            </div>
+            <div class="inner-about">
+                <div class="inner-about-image">
+                    <img src="images/chiness.jpg" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div id="services" class="our-services">
+        <div class="service-content">
+            <div class="left-service-content">
+                <h1>Our special services</h1>
+                <p>At SelfCare360, we understand that mental wellness is a deeply personal journey, and no two
+                    individuals
+                    are alike. That's why we're pleased to offer a range of specialized services tailored to address
+                    your unique needs and preferences. <br>
+                    These services are designed to empower you on your path to self-discovery, healing, and growth.</p>
+            </div>
+            <div class="right-service-content">
+                <div class="right-btn">
+                </div>
+            </div>
+        </div>
+        <div class="main-services">
+            <div class="inner-services-content">
+                <div class="service-icon">
+                    <i class="fas fa-notes-medical"></i>
+                </div>
+                <h2>Online Therapy and Counseling</h2>
+                <p>Offer remote therapy and counseling sessions for individuals who prefer to seek help from the comfort
+                    of their homes.<br>
+                    Highlight the convenience, confidentiality, and effectiveness of online therapy.</p>
+            </div>
+            <div class="inner-services-content">
+                <div class="service-icon">
+                    <i class="fas fa-hospital-user"></i>
+                </div>
+                <h2>Wellness <br>Workshops</h2>
+                <p>Conduct virtual and in-person wellness workshops on various topics such as stress management,
+                    mindfulness, emotional intelligence, and resilience.
+                    <br>
+                    Provide options for both one-time workshops and multi-session courses.
+                </p>
+            </div>
+            <div class="inner-services-content">
+                <div class="service-icon">
+                    <i class="fas fa-user-md"></i>
+                </div>
+                <h2>Personalized<br> Self-Care Plans</h2>
+                <p>Offer individualized self-care plans developed by mental health experts,taking into account each
+                    client's unique needs and goals.<br>
+                    Stress the importance of a tailored approach to mental wellness.</p>
+            </div>
+        </div>
+    </div>
+
+    <div id="gallery" class="gallery">
+        <h1>Gallery</h1>
+        <div class="main-gallery">
+            <div class="inner-gallery">
+                <img src="images/gal1.jpg" alt="">
+            </div>
+            <div class="inner-gallery">
+                <img src="images/gal2.jpg" alt="">
+            </div>
+            <div class="inner-gallery">
+                <img src="images/gal3.jpg" alt="">
+            </div>
+            <div class="inner-gallery">
+                <img src="images/gal4.jpg" alt="">
+            </div>
+            <div class="inner-gallery">
+                <img src="images/gal5.jpg" alt="">
+            </div>
+            <div class="inner-gallery">
+                <img src="images/gal6.jpg" alt="">
+            </div>
+        </div>
+    </div>
+
+    <div id="contact" class="footer">
+        <div class="main-footer">
+            <div class="inner-footer-c">
+                <h2><i class='bx bxs-contact'></i> Contact</h2>
+                <a href="tel:+94750802365"><i class='bx bxs-phone'></i> +94 77 0802 365</a>
+                <a href="tel:+94750802365"><i class='bx bxl-whatsapp'></i> +94 75 0802 353</a>
+                <a href="https://maps.windows.com/?form=WNAMSH&collection=point.7.284186_81.860115_new%20road%2C%20Oluvil%2C%20Sri%20Lanka"><i class='bx bxs-map'></i> Oluvil - Sri Lanka</a>
+                <a href="mailto:rusa.rock72@gmill.com"><i class='bx bxl-gmail'></i> rusa.rock72@gmail.com</a>
+            </div>
+            <div class="inner-footer-m">
+                <h2><i class='bx bx-world'></i> Social Media</h2>
+                <a href="https://web.facebook.com/profile.php?id=100010893247458"><i class='bx bxl-facebook-circle'></i> Facebook</a>
+                <a href="https://www.instagram.com/muhammadhrusaith?igsh=YTQwZjQ0NmI0OA=="><i class='bx bxl-instagram-alt'></i> Instagram</a>
+                <a href="https://www.linkedin.com/in/muhammadh-rusaith-38aa75248/"><i class='bx bxl-linkedin'></i> Linkdin</a>
+                <a href="https://t.me/rusaith"><i class='bx bxl-telegram'></i> Telegram</a>
+            </div>
+        </div>
+    </div>
+    
+    <div class="name-footer">
+        <h2>© Created by MIM.Rusaith | @Alrights reserved |
+    </div>
+
+    <script src="script.js"></script>
+</body>
+
+</html>
