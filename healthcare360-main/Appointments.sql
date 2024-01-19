69492ababeed68dfd64600f1fba4bcf32cbf7946
RusaithMhd <111362210+RusaithMhd@users.noreply.github.com>
2024-01-20 00:51:24 +0530

    Delete healthcare360-main directory

--- healthcare360-main/Appointments.sql
+++
@@ -1,7 +0,0 @@
--- Database schema for `rusaith`
-CREATE TABLE appointments (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_name_ VARCHAR(255),
-    Phone_Number VARCHAR(15),
-    Email VARCHAR(255)
-);
--- healthcare360-main/dbc.php
+++
@@ -1,9 +0,0 @@
-<?php
-$con = mysqli_connect("localhost","root","","project_244");
-
-if ($con == false) {
-    die("CONNECTION ERROR". mysqli_connect_error());
-}
-
-?>
-
